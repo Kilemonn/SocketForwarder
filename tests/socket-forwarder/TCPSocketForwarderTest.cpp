35f9bc4ba6540eb8204a5641f5ff331a12bf5bd5
Kilemonn <kylegonzalez2255@gmail.com>
2024-07-07 21:31:32 +0900

    Add third client to TCP test in a different group and make sure they don't receive any messages.

--- tests/socket-forwarder/TCPSocketForwarderTest.cpp
+++ tests/socket-forwarder/TCPSocketForwarderTest.cpp
@@ -37,13 +37,19 @@ namespace forwarder
 		std::string groupId = "98235123";
 		kt::TCPSocket client1("localhost", serverSocket.getPort());
 		ASSERT_TRUE(client1.send(NEW_CLIENT_PREFIX_DEFAULT + groupId));
-
 		std::this_thread::sleep_for(10ms);
+
 		ASSERT_TRUE(tcpGroupWithIdExists(groupId));
 
 		kt::TCPSocket client2("localhost", serverSocket.getPort());
 		ASSERT_TRUE(client2.send(NEW_CLIENT_PREFIX_DEFAULT + groupId));
-		std::this_thread::sleep_for(20ms);
+		std::this_thread::sleep_for(10ms);
+
+		std::string client3GroupId = "112343";
+		ASSERT_NE(client3GroupId, groupId);
+		kt::TCPSocket client3("localhost", serverSocket.getPort());
+		ASSERT_TRUE(client3.send(NEW_CLIENT_PREFIX_DEFAULT + client3GroupId));
+		std::this_thread::sleep_for(10ms);
 
 		std::string toSend = "TCPSocketForwarderTest";
 		ASSERT_TRUE(client1.send(toSend));
@@ -51,6 +57,8 @@ namespace forwarder
 
 		std::string received = client2.receiveAmount(50);
 		ASSERT_EQ(toSend, received);
+		// Make sure client 3 did not receive anything since its not in that group
+		ASSERT_FALSE(client3.ready());
 
 		received += "12738651239132434";
 		ASSERT_TRUE(client2.send(received));
@@ -58,8 +66,11 @@ namespace forwarder
 
 		toSend = client1.receiveAmount(50);
 		ASSERT_EQ(toSend, received);
+		// Make sure client 3 did not receive anything since its not in that group
+		ASSERT_FALSE(client3.ready());
 
 		client1.close();
 		client2.close();
+		client3.close();
 	}
 }
