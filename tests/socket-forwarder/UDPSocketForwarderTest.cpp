9480bba584da5f12f429e5a6af8d2936dd9168c5
Kilemonn <kylegonzalez2255@gmail.com>
2024-07-08 12:38:06 +0900

    Add comments and fix assertions in UDP test. Use "127.0.0.1" in UDP test since the application wasn't receiving UDP messages in the alpine image for some reason.

--- tests/socket-forwarder/UDPSocketForwarderTest.cpp
+++ tests/socket-forwarder/UDPSocketForwarderTest.cpp
@@ -35,61 +35,57 @@ namespace forwarder
 	{
         ASSERT_EQ(0, udpGroupMemberCount());
 
+        // Add client 1 to the group
 		kt::UDPSocket client1;
-        client1.bind();
-        std::pair<bool, std::pair<int, kt::SocketAddress>> sendResult = client1.sendTo("localhost", udpSocket.getListeningPort().value(), NEW_CLIENT_PREFIX_DEFAULT + std::to_string(client1.getListeningPort().value()));
-		std::cout << "Client 1 send result: " << sendResult.second.first << std::endl;
-        ASSERT_TRUE(sendResult.first);
-		std::this_thread::sleep_for(100ms);
+        client1.bind(); // Bind since we will recieve data later
+        ASSERT_TRUE(client1.sendTo("127.0.0.1", udpSocket.getListeningPort().value(), NEW_CLIENT_PREFIX_DEFAULT + std::to_string(client1.getListeningPort().value())).first);
+		std::this_thread::sleep_for(10ms);
 
+        // Check group count is incremented since client 1 has joined
 		ASSERT_EQ(1, udpGroupMemberCount());
 
+        // Add client 2 to the group
 		kt::UDPSocket client2;
-        client2.bind();
-        ASSERT_TRUE(client2.sendTo("localhost", udpSocket.getListeningPort().value(), NEW_CLIENT_PREFIX_DEFAULT + std::to_string(client2.getListeningPort().value())).first);
-		std::this_thread::sleep_for(100ms);
+        client2.bind(); // Bind since we will recieve data later
+        ASSERT_TRUE(client2.sendTo("127.0.0.1", udpSocket.getListeningPort().value(), NEW_CLIENT_PREFIX_DEFAULT + std::to_string(client2.getListeningPort().value())).first);
+		std::this_thread::sleep_for(10ms);
 
+        // Check group count is incremented since client 2 has joined
         ASSERT_EQ(2, udpGroupMemberCount());
 
+        // Send to group from client1
 		std::string toSend = "UDPSocketForwarderTest";
-		ASSERT_TRUE(client1.sendTo("localhost", udpSocket.getListeningPort().value(), toSend).first);
-		std::this_thread::sleep_for(1000ms);
+		ASSERT_TRUE(client1.sendTo("127.0.0.1", udpSocket.getListeningPort().value(), toSend).first);
+		std::this_thread::sleep_for(10ms);
 
-        ASSERT_TRUE(client2.ready() || client1.ready());
+        // Check it is received at client 2
+        ASSERT_TRUE(client2.ready());
+        std::pair<std::optional<std::string>, std::pair<int, kt::SocketAddress>> readResult = client2.receiveFrom(50);
+        ASSERT_NE(-1, readResult.second.first);
+        ASSERT_EQ(toSend, readResult.first.value());
 
-        if (client2.ready())
-        {
-            std::pair<std::optional<std::string>, std::pair<int, kt::SocketAddress>> result = client2.receiveFrom(50);
-            ASSERT_NE(-1, result.second.first);
-            ASSERT_EQ(toSend, result.first.value());
-        }
-
-        if (client1.ready())
-        {
-            std::pair<std::optional<std::string>, std::pair<int, kt::SocketAddress>> result = client1.receiveFrom(50);
-            ASSERT_NE(-1, result.second.first);
-            ASSERT_EQ(toSend, result.first.value());
-        }
+        // Check it is received at client 1 because the UDP group does not know who sent it and just forwards to everyone
+        ASSERT_TRUE(client1.ready());
+        readResult = client1.receiveFrom(50);
+        ASSERT_NE(-1, readResult.second.first);
+        ASSERT_EQ(toSend, readResult.first.value());
         
+        // Send different message from client 2
 		toSend += "99283647561";
-		ASSERT_TRUE(client2.sendTo("localhost", udpSocket.getListeningPort().value(), toSend).first);
-		std::this_thread::sleep_for(1000ms);
+		ASSERT_TRUE(client2.sendTo("127.0.0.1", udpSocket.getListeningPort().value(), toSend).first);
+		std::this_thread::sleep_for(10ms);
 
-        ASSERT_TRUE(client2.ready() || client1.ready());
+        // Check client 1 received it
+        ASSERT_TRUE(client1.ready());
+        readResult = client1.receiveFrom(50);
+        ASSERT_NE(-1, readResult.second.first);
+        ASSERT_EQ(toSend, readResult.first.value());
 
-        if (client1.ready())
-        {
-            std::pair<std::optional<std::string>, std::pair<int, kt::SocketAddress>> result = client1.receiveFrom(50);
-            ASSERT_NE(-1, result.second.first);
-            ASSERT_EQ(toSend, result.first.value());
-        }
-
-        if (client2.ready())
-        {
-            std::pair<std::optional<std::string>, std::pair<int, kt::SocketAddress>> result = client2.receiveFrom(50);
-            ASSERT_NE(-1, result.second.first);
-            ASSERT_EQ(toSend, result.first.value());
-        }
+        // Again since its a broadcast client 2 will also receive it
+        ASSERT_TRUE(client2.ready());
+        readResult = client2.receiveFrom(50);
+        ASSERT_NE(-1, readResult.second.first);
+        ASSERT_EQ(toSend, readResult.first.value());
 
 		client1.close();
 		client2.close();
